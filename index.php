6e9f431abcabbd140fa70a8d3090c8dca336dfc2
Jakub <jakubmisiurka@gmail.com>
2023-12-12 20:35:51 +0100

    .

--- index.php
+++ index.php
@@ -11,35 +11,37 @@ $tool_types = $conn->query($sql);
   <h2>Wybierz narzędzie</h2><br><br>
   <form name="form" action="wybor_zestawu.php" method="post">
     <select name="tool">
-    <?php
-      while($tool_type = $tool_types->fetch_assoc()) { ?>  
-      <option value="<?php echo $tool_type["tool_type"]?>"><?php echo $tool_type["tool_type"] ?></option>
+      <?php
+      while ($tool_type = $tool_types->fetch_assoc()) { ?>
+        <option value="<?php echo $tool_type["tool_type"] ?>">
+          <?php echo $tool_type["tool_type"] ?>
+        </option>
       <?php } ?>
 
     </select>
-    <input type="submit" value="Wybierz" class="center-button"/>
+    <input type="submit" value="Wybierz" class="center-button" />
   </form>
 
-<?php 
-$sql = "SELECT DISTINCT materiał
+  <?php
+  $sql = "SELECT DISTINCT materiał
         FROM tool
         ORDER BY materiał";
-$materials = $conn->query($sql);
-?>
-  
-  <b>Wybierz materiał</b><br><br>
+  $materials = $conn->query($sql);
+  ?>
+
+  <h2>Wybierz materiał</h2><br><br>
   <form name="form" action="wybor_mat.php" method="post">
     <select name="materiał">
       <?php
-      while($material = $materials->fetch_assoc()) {?>
-      <option value="<?php echo $material["materiał"]?>"><?php echo $material["materiał"] ?></option>
+      while ($material = $materials->fetch_assoc()) { ?>
+        <option value="<?php echo $material["materiał"] ?>">
+          <?php echo $material["materiał"] ?>
+        </option>
       <?php } ?>
 
     </select>
-    <input type="submit" value="Wybierz" class="center-button"/>
+    <input type="submit" value="Wybierz" class="center-button" />
   </form>
-
-
 </div>
 
 <?php
@@ -47,29 +49,37 @@ $sql = "SELECT foto, id, tool_type, materiał,qr FROM tool";
 $result = $conn->query($sql);
 ?>
 
-<?php
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) { ?>
-	 <table width="30%" cellspacing="0" cellpadding="0" border="1">
-	 <tr>
-		<td width="70" align="center"><img src="foto/<?php echo $row["foto"] ?>" height="70" width="70" border="0" alt="[Rozmiar: 4189 bajtów]"></td>
-		<td width = "30 " align="center"><a href='narzedzie_par.php?id=<?php echo $row["id"];?>' class="link_do_narz"><?php echo $row["id"];?></a></td>
-		<td width = "90 "align="left"> <?php echo $row["tool_type"]; ?></td>
-		<td width = "80 "> <?php echo $row["materiał"]; ?></td>
-		<td width = "80 " align ="center">  <img src="foto/<?php echo $row["qr"] ?>" height="75" width="75" border="0" alt="[Rozmiar: 4189 bajtów]"> </td> 
-	</tr>
-	</table>
-	
+  <?php
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) { ?>
+      <table align="center" width="50%" cellspacing="0" cellpadding="0" border="1">
+        <tr>
+          <td width="100" align="center"><img src="foto/<?php echo $row["foto"] ?>" height="70" width="70" border="0"
+              alt="[Rozmiar: 4189 bajtów]"></td>
+          <td width="50" align="center"><a href='narzedzie_par.php?id=<?php echo $row["id"]; ?>' class="link_do_narz">
+              <?php echo $row["id"]; ?>
+            </a></td>
+          <td width="100" align="center">
+            <?php echo $row["tool_type"]; ?>
+          </td>
+          <td width="80">
+            <?php echo $row["materiał"]; ?>
+          </td>
+          <td width="80" align="center"> <img src="foto/<?php echo $row["qr"] ?>" height="75" width="75" border="0"
+              alt="[Rozmiar: 4189 bajtów]"> </td>
+        </tr>
+      </table>
 
-<?PHP
+    <?PHP
+    }
+  } else {
+    echo "0 results";
   }
-} else {
-  echo "0 results";
-}
-$conn->close();
+  $conn->close();
 
-?>
+  ?>
 
-    </body>
+</body>
+
 </html>
