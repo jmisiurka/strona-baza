3c67b01c19c3ed85cd1eb7c39d09ee40319291cb
Mateyszszszszsz <matisikorski2001@o2.pl>
2023-12-12 21:47:29 +0100

    .

--- narzedzie_par.php
+++ narzedzie_par.php
@@ -8,20 +8,20 @@ $sql  = "SELECT * FROM tool JOIN set_id ON tool.id_set = set_id.id JOIN cladding
 
 
 
-$result = $conn->query($sql);echo "Connected successfully";
+$result = $conn->query($sql);
 $row = mysqli_fetch_assoc($result);
 ?>
 
 
-<h1><?php echo $row["tool_type"]; ?><h1><br>
+<h1><b><?php echo $row["tool_type"]; ?></b><h1><br>
 
-<table>
+<table class="tablica" align="center"  cellspacing="0" cellpadding="10" border="15">
 <tr><td width = "180"> <img src="foto/<?php echo $row["foto"] ?>" height="70" width="70" border="0" alt="[Rozmiar: 4189 bajtów]"></td>
 <td width="80"><img src="foto/<?php echo $row["qr"] ?>" height="75" width="75" border="0" alt="[Rozmiar: 4189 bajtów]"></td></tr>
 </table>
 <br>
 
-<table cellspacing="0" cellpadding="1" border="1">
+<table class="tablica" cellspacing="0" cellpadding="1" border="5" align="center">
 <tr><td width = "180"><b>Id</b></td><td width="80"><?php  echo $row["id"];?></td></tr>
 <tr><td><b>Ile regeneracji</b></td><td><?php  echo $row["reg_cycles"]?></td></tr>
 <tr><td><b>Drut</b></td><td><?php  echo $row["wire"]?></td></tr>
@@ -40,7 +40,7 @@ $row = mysqli_fetch_assoc($result);
 $sql  = "SELECT * FROM tool JOIN set_id ON tool.id_set = set_id.id JOIN cladding_param ON tool.id_cladd_prog = cladding_param.id where tool.id = '$id'";
 ?>
 
-<table cellspacing="0" cellpadding="1" border="1">
+<table class="tablica" cellspacing="0" cellpadding="1" border="5" align="center">
 
 <tr><td width = "180"><b>Numer Kompletu</b></td><td width="80"><?php  echo $row["id_number"]?></td></tr>
 <tr><td><b>Nazwa Kompletu</b></td><td><?php  echo $row["nazwa_kompletu"]?></td></tr>
